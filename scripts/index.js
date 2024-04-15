bb2ed7faf7705154061b8f7daf1795c8c50882e4
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-04-15 18:24:10 -0400

    Create deck of cards template, udpate styles for cards, add functionality to create a deck of cards via Javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
 // const gameDescriptionElement = document.querySelector(".game__collection_hidden");
+
+const cardDeckList = document.querySelector(".deck__card-list");
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".deck__card");;
+
+function createCard(number) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector(".deck__card-image");
+
+    cardImage.src = `/images/Card-image-updated-names/Card-${number}.svg`;
+    cardImage.value = number;
+    return cardElement
+}
+
+function createDeckOfCards() {
+    let cardElement = {}
+    for (let i=1; i<=52; i++) {
+        cardElement = createCard(i);
+        cardDeckList.append(cardElement);
+    }
+    
+}
+
+createDeckOfCards();
